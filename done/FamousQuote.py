f0707b756c0fa7b693910e76006e26517c9b2cf4
Kasper B <kasperbosteels@hotmail.com>
2023-01-30 17:15:51 +0100

    renamed file so its better organized

--- done/FamousQuote.py
+++
@@ -1,4 +0,0 @@
-famous_person = " Kasper Bosteels"
-quote = "C'est la vie "
-print(f"{famous_person.lstrip()} once said: \"{quote.rstrip()}\"")
-exit()
--- done/documentone.py
+++
@@ -1,5 +0,0 @@
-valueone = 12
-valuetwo = 20
-textone = " bacon burgers pls...."
-print(f"{valueone+valuetwo}{textone}") 
-exit()
