56f6f60b7f52aa57a44827f26988dda4a0f5ffc0
Kasper B <kasperbosteels@hotmail.com>
2023-01-30 17:52:16 +0100

    Functions with variables

---
+++ Basic Functions/Functions with Variables.py
@@ -0,0 +1,8 @@
+def Make_Shirt(text='I love python',size="L"):
+    print(f"Making a new {size} shirt with '{text}' in text on the front.")
+Make_Shirt()
+Make_Shirt(size="XL", text="Snake Programming Language")
+
+def Describe_City(city="Reykavik",country="Is the capital of Iceland"):
+    print(f"{city}\n{country}")
+Describe_City()
