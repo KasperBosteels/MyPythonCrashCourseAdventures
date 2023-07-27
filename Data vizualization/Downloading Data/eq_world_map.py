df5a485936c2123d26ac3ef8f19dabd419afe0b2
Kasper B <kasperbosteels@hotmail.com>
2023-07-27 16:41:20 +0200

    earthquake world map finished

--- Data vizualization/Downloading Data/eq_world_map.py
+++ Data vizualization/Downloading Data/eq_world_map.py
@@ -4,7 +4,7 @@ from plotly import offline
 
 
 
-filename = "..\Data\eq_1_day_m1.json"
+filename = "..\Data\eq_30_days_m1.json"
 
 with open(filename, "r", encoding='utf-8') as f:
     all_eq_data = json.load(f)
@@ -13,12 +13,14 @@ with open (readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
 
 all_eq_dicts = all_eq_data['features']
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    hover_text = eq_dict['properties']['title']
+    hover_texts.append(hover_text)
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
@@ -26,8 +28,13 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
-        "size": [5*mag for mag in mags],
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
     },
 }]
 my_layout = Layout(title="Global Earthquakes")
